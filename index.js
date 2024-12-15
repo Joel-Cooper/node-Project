a6542b5765f9332f224fcdacb2d5e5056ed3c297
Joel Cooper <161271563+Joel-Cooper@users.noreply.github.com>
2024-12-16 10:42:32 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 import { app } from './server.js'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-
-dotenv.config()
-console.log('MongoDB URI:', process.env.MONGODB_URI);  
 
 const port = process.env.PORT
 
